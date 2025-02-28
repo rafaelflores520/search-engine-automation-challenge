c8e7dcfd5739a52ce3c054457aef909b27a1e543
Rafael Flores <rafael.flores0801@gmail.com>
2025-02-27 18:44:19 -0600

    Search and validation of result based on query, some issues with origin change working on the resolution

--- cypress/e2e/google-search.cy.js
+++ cypress/e2e/google-search.cy.js
@@ -1,1 +1,16 @@
-describe("Search on Google", function () {});
+const querys = require("../fixtures/example.json");
+
+describe("Search on Google", function () {
+  querys.forEach(function (query) {
+    it(`User is able to get to ${query.q}`, function () {
+      cy.visit("https://duckduckgo.com/");
+      // cy.get("textarea[id='APjFqb']").type(`${query}\n`); Google version
+      cy.get("input[id='searchbox_input']").type(`${query.q}\n`);
+      cy.get("a[data-testid='result-title-a']")
+        .should("exist")
+        .first()
+        .as("firstResult");
+      cy.get("@firstResult").click();
+    });
+  });
+});
